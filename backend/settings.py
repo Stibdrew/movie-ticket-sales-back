f813737868fdc6a89883a6cb49d8759a1278f5b3
STEVEN RAQUEDAN <scraquedan1@student.hau.edu.ph>
2025-03-28 16:16:30 +0800

    damn

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,10 @@ import dj_database_url
 import django_heroku
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
